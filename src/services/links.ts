c1deb6e9af4f1a5e325b964969957360e942ff3f
sergio <sergimonti10@gmail.com>
2024-06-20 14:23:52 +0200

    link

--- src/services/links.ts
+++ src/services/links.ts
@@ -1,15 +1,19 @@
-export const IMAGE_USERS_BASE_URL = 'http://127.0.0.1:8000/storage/img_users/';
-export const IMAGE_PRODUCTS_BASE_URL = 'http://127.0.0.1:8000/storage/img_products/';
-export const PRODUCTS_BASE_URL = 'http://127.0.0.1:8000/api/products';
-export const PRODUCT_ID_BASE_URL = 'http://127.0.0.1:8000/api/products/';
-export const CUTS_BASE_URL = 'http://127.0.0.1:8000/api/cuts';
-export const CUT_ID_BASE_URL = 'http://127.0.0.1:8000/api/cuts/';
-export const USERS_BASE_URL = 'http://127.0.0.1:8000/api/users';
-export const USER_ID_BASE_URL = 'http://127.0.0.1:8000/api/users/';
-export const ORDERS_BASE_URL = 'http://127.0.0.1:8000/api/orders';
-export const DOWNLOAD_PDF_BASE_URL = 'http://127.0.0.1:8000/api/generate-invoice'
-export const AUTH_BASE_URL = 'http://127.0.0.1:8000/api/';
-export const REVIEWS_BASE_URL = 'http://127.0.0.1:8000/api/reviews';
-export const BASE_URL = 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = 'https://apipatata.silverproyectspain.com';
+// const API_BASE_URL = 'https://patataapi.silverprojectspain.com';
+
+// http://localhost:8000
 
 
+export const IMAGE_USERS_BASE_URL = `${API_BASE_URL}/storage/img_users/`;
+export const IMAGE_PRODUCTS_BASE_URL = `${API_BASE_URL}/storage/img_products/`;
+export const PRODUCTS_BASE_URL = `${API_BASE_URL}/api/products`;
+export const PRODUCT_ID_BASE_URL = `${API_BASE_URL}/api/products/`;
+export const CUTS_BASE_URL = `${API_BASE_URL}/api/cuts;`
+export const CUT_ID_BASE_URL = `${API_BASE_URL}/api/cuts/`;
+export const USERS_BASE_URL = `${API_BASE_URL}/api/users`;
+export const USER_ID_BASE_URL = `${API_BASE_URL}/api/users/`;
+export const ORDERS_BASE_URL = `${API_BASE_URL}/api/orders`;
+export const DOWNLOAD_PDF_BASE_URL = `${API_BASE_URL}/api/generate-invoice`;
+export const AUTH_BASE_URL = `${API_BASE_URL}/api/`;
+export const REVIEWS_BASE_URL = `${API_BASE_URL}/api/reviews`;
+export const BASE_URL = `${API_BASE_URL}/api/`;
